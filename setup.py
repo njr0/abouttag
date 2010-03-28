d29c0f5d9076de1b0f4f735680328dc07a92ae58
Nicholas J. Radcliffe <njr@StochasticSolutions.com>
2010-03-28 11:18:49 +0100

    Updaded version number --- should have done that before.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='abouttag',
-      version='0.1',
+      version='0.2',
       description='Normalized FluidDB about tags',
       author='Nicholas J. Radcliffe',
       author_email='njr@StochasticSolutions.com',
