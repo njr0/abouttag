d30a8ccea4b1fe3d8bebafe161205f46ad000c20
Nicholas J. Radcliffe <njr@StochasticSolutions.com>
2011-07-18 13:41:24 +0100

    Added extra tests (bad cases) to film.

--- abouttag/film.py
+++ abouttag/film.py
@@ -28,8 +28,8 @@ def film(title, year, **kwargs):
         assert kwargs['convention'].lower() == u'track-u'
     if type(year) == int:
         year = str(year)
-        assert len(year) == 4
-        assert all('0' <= y <= '9' for y in year)
+    assert len(year) == 4
+    assert all('0' <= y <= '9' for y in year)
 
     return normalize_film(title, year, **kwargs)
 
@@ -37,17 +37,26 @@ movie = film
 
 
 class TestFilm(about.AboutTestCase):
-    def testFluidDBNormalizeTrack(self):
+    def testFluidDBNormalizeFilm(self):
         expected = (
             ((u"Citizen Kane", u'1941'), u'film:citizen kane (1941)'),
             ((u"The Last Seduction", 1994), u'film:the last seduction (1994)'),
             ((u"Él", 1953), u'film:él (1953)'),
             ((u"L'Âge d'Or", u'1930'), u'film:lâge dor (1930)'),
         )
+        bads = ((u"Citizen Kane", u''),
+                (u"Citizen Kane", u'1941-42'),
+                (u"Citizen Kane", u'41')
+        )
         for (input, output) in expected:
-            title, year= input[0], input[1]
+            title, year = input[0], input[1]
             self.assertEqual((input, output), (input, film(title, year)))
             self.assertEqual((input, output), (input, movie(title, year)))
+        for input in bads:
+            title, year = input[0], input[1]
+            self.assertRaises(AssertionError, film, title, year)
+
+
                              
 
 if __name__ == '__main__':
